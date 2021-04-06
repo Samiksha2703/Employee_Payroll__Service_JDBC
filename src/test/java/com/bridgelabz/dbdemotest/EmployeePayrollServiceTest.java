0119faf022fcc77a4d53923bce19f2060fb46c07
Samiksha <shende.samiksha@rediffmail.com>
2021-04-06 19:55:08 +0530

    [Samiksha].ADD code to test the retrieve data from database employee_service1

--- src/test/java/com/bridgelabz/dbdemotest/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/dbdemotest/EmployeePayrollServiceTest.java
@@ -1,2 +1,21 @@
-package com.bridgelabz.dbdemotest;public class EmployeePayrollServiceTest {
+package com.bridgelabz.dbdemotest;
+
+import com.bridgelabz.dbdemo.EmployeePayrollData;
+import com.bridgelabz.dbdemo.EmployeePayrollService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class EmployeePayrollServiceTest {
+    EmployeePayrollService employeePayrollService;
+    List<EmployeePayrollData> employeePayrollList;
+
+    @Test
+    public void given3EmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount(){
+        employeePayrollService = new EmployeePayrollService();
+        employeePayrollList = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
+        Assertions.assertEquals(3, employeePayrollList.size());
+    }
+
 }
