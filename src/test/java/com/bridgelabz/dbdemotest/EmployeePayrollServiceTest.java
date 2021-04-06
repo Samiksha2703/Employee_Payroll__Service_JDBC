b8de63741ed7acd01b40d1c1d154214a35aff153
Samiksha <shende.samiksha@rediffmail.com>
2021-04-06 22:20:31 +0530

    [Samiksha].ADD code to test the updated salary in database

--- src/test/java/com/bridgelabz/dbdemotest/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/dbdemotest/EmployeePayrollServiceTest.java
@@ -12,10 +12,19 @@ public class EmployeePayrollServiceTest {
     List<EmployeePayrollData> employeePayrollList;
 
     @Test
-    public void given3EmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount(){
+    public void given3EmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
         employeePayrollService = new EmployeePayrollService();
         employeePayrollList = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
         Assertions.assertEquals(3, employeePayrollList.size());
+    }
+
+    @Test
+    public void givenNewSalaryForEmployee_WhenUpdated_ShouldMatch() {
+        employeePayrollService = new EmployeePayrollService();
+        employeePayrollList = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
+        employeePayrollService.updateEmployeeSalary("Terisa", 3000000.0);
+        boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Terisa");
+        Assertions.assertTrue(result);
     }
 
 }
