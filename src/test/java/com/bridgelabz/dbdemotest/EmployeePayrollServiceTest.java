09253bed1a32722e3b8dc84e5811ea4e78eac5a2
Samiksha <shende.samiksha@rediffmail.com>
2021-04-08 18:19:23 +0530

    [Samiksha].ADD code for test to compare object and database with prepared statement when update database

--- src/test/java/com/bridgelabz/dbdemotest/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/dbdemotest/EmployeePayrollServiceTest.java
@@ -5,6 +5,7 @@ import com.bridgelabz.dbdemo.EmployeePayrollService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class EmployeePayrollServiceTest {
@@ -27,4 +28,12 @@ public class EmployeePayrollServiceTest {
         Assertions.assertTrue(result);
     }
 
+    @Test
+    public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDB() {
+        employeePayrollService = new EmployeePayrollService();
+        employeePayrollList = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
+        employeePayrollService.updateEmployeeSalary("Terisa", 3000000.0);
+        boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Terisa");
+        Assertions.assertTrue(result);
+    }
 }
